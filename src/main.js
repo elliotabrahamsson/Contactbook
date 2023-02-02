f781c62dd302f87ef4402b86553301cd07c630b3
isacchr <isac.christiansson@gmail.com>
2023-02-02 11:11:13 +0100

    Setup with vue, Alive

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/main.css";
+
+createApp(App).mount("#app");
+
+/*
+getContact = () => {
+  const XHR = new XMLHttpRequest();
+
+  XHR.onload = () => {};
+  XHR.open("GET", "/contacts");
+  XHR.send();
+}; */
