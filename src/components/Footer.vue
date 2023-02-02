9bed380a8e59b774ceba2c68f92e26c650e86ad8
CasperHogstrom <casperhogstrom3@gmail.com>
2023-02-02 14:17:38 +0100

    Changed the look of homeView

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,5 @@
 <template>
-    <div>
+    <div class=" absolute inset-x-0 bottom-0">
         <img src="../assets/footerClouds.svg" alt="Clouds in footer">
     </div>
 </template>
-
-
-
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template class="background">
-  <div>
-    <h1>Contactbook</h1>
+  <div class=" w-fit mx-auto grid grid-cols-1 gap-4">
+    <h1 class=" mx-1">Contactbook</h1>
 
-    <input id="firstName" placeholder="Förnamn:" type="text" />
-    <input id="lastName" placeholder="Efternamn:" type="text" />
-    <input type="text" id="email" placeholder="Email:" />
+    <input id="firstName" placeholder="Förnamn:" type="text" class=" " />
+    <input id="lastName" placeholder="Efternamn:" type="text" class=" " />
+    <input type="text" id="email" placeholder="Email:" class=" " />
     <button id="add" v-on:click="addContact()">Lägg till</button>
     <button id="showAll" v-on:click="showContacts()">
       Visa alla kontakter
