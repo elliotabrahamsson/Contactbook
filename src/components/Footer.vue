421f81f76e011a3a089733378a7d85ac2d06f097
axelhagerback <axel.hagerback@gmail.com>
2023-02-03 12:48:23 +0100

    made footer sticky

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" absolute inset-x-0 bottom-0">
-        <img src="../assets/footerClouds.svg" alt="Clouds in footer">
-    </div>
+  <div class="sticky bottom-0">
+    <img src="../assets/footerClouds.svg" alt="Clouds in footer" />
+  </div>
 </template>
